83b5684acdf577de97f7b7d100032d631e67386d
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-17 02:36:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,23 +6,22 @@ echo "Iniciando instalação do SlowDNS via Xray Manager Mult SlowDNS..."
 mkdir -p /etc/slowdns
 cd /etc/slowdns || exit
 
-# Baixar todos os scripts do repositório
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/slowdns
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/slowdns-ssh
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/slowdns-ssl
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/slowdns-drop
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/slowdns-socks
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/slowdns-info
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/startdns
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/restartdns
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/stopdns
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/remove-slow
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/server.key
-wget -N https://raw.githubusercontent.com/SeuUsuario/xray-manager-mult-slowdns/main/server.pub
+# Baixar todos os scripts da raiz do repo
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/slowdns-ssh
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/slowdns-ssl
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/slowdns-drop
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/slowdns-socks
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/slowdns-info
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/startdns
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/restartdns
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/stopdns
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/remove-slow
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/server.key
+wget -N https://raw.githubusercontent.com/miau4/xray-manager-mult-slowdns/main/server.pub
 
 # Permissões
 chmod +x /etc/slowdns/*
-cp /etc/slowdns/slowdns /usr/local/bin/slowdns
+cp /etc/slowdns/slowdns-ssh /usr/local/bin/slowdns
 chmod +x /usr/local/bin/slowdns
 
 echo "Instalação concluída!"
